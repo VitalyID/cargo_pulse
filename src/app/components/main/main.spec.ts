0b3c2c2255f313384f8b0db6674313e85360519f
Vitaly <s25698@ya.ru>
2025-09-12 02:55:37 +0500

    test is done

--- src/app/components/main/main.spec.ts
+++ src/app/components/main/main.spec.ts
@@ -5,19 +5,35 @@ import { Main } from './main';
 describe('Main', () => {
   let component: Main;
   let fixture: ComponentFixture<Main>;
+  let compiled: HTMLElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Main]
-    })
-    .compileComponents();
+      declarations: [Main],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(Main);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    compiled = fixture.nativeElement as HTMLElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should be create b header element', () => {
+    const header = compiled.querySelector('header');
+    expect(header).toBeTruthy;
+  });
+
+  it('should be create a body', () => {
+    const main = compiled.querySelector('main');
+    expect(main).toBeTruthy;
+  });
+
+  it('should be created a footer', () => {
+    const footer = compiled.querySelector('footer');
+    expect(footer).toBeTruthy;
   });
 });
